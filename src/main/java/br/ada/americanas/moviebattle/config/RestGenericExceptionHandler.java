456a1b8d1b6493cb971a0dd4bfb1bb1ad591d3f3
georgrybski <rybskigeorg@gmail.com>
2023-03-12 07:01:28 -0300

    Added RestGenericExceptionHandler

---
+++ src/main/java/br/ada/americanas/moviebattle/config/RestGenericExceptionHandler.java
@@ -0,0 +1,92 @@
+package br.ada.americanas.moviebattle.config;
+
+import br.ada.americanas.moviebattle.exceptions.BattleNotFoundExeption;
+import br.ada.americanas.moviebattle.exceptions.MovieNotFoundException;
+import br.ada.americanas.moviebattle.exceptions.PlayerNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice(annotations = {RestController.class})
+public class RestGenericExceptionHandler {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    public ResponseEntity<Object> handlerConstraintViolationException(
+            ConstraintViolationException ex,
+            WebRequest request
+    ) {
+        logger.error("Unhandled exception", ex);
+
+        List<Map<String, Object>> errors = new ArrayList<>();
+        for (ConstraintViolation error : ex.getConstraintViolations()) {
+            errors.add(
+                    Map.of(
+                            error.getPropertyPath().toString(),
+                            error.getMessage()
+                    )
+            );
+        }
+
+        Map<String, Object> body = Map.of(
+                "code", HttpStatus.BAD_REQUEST,
+                "errors", errors,
+                "date-time", LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handlerAnyException(
+            Exception ex,
+            WebRequest request
+    ) {
+        logger.error("Unhandled exception", ex);
+
+        Map<String, Object> body = Map.of(
+                "code", HttpStatus.INTERNAL_SERVER_ERROR,
+                "message", "Sorry, we failed with you. Try again later.....",
+                "date-time", LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(body);
+    }
+
+    @ExceptionHandler(PlayerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handlePlayerNotFoundException(PlayerNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(BattleNotFoundExeption.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleBattleNotFoundException(BattleNotFoundExeption exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MovieNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleMovieNotFoundException(MovieNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+}
